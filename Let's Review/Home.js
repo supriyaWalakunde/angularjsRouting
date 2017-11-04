fe0eac0c48f19864ff4887e7bde253b82e6a1c66
Supriya <supriya.ps29@gmail.com>
2017-11-04 22:37:07 +0530

    delete the file

--- Let's Review/Home.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by APPLE on 04-11-2017.
- */
-
-var app=angular.module("myapp",[]);
-app.controller('myctrl',function($script){
-    $scope.names = ["ambiences", "Tobias", "Linus"];
-})
