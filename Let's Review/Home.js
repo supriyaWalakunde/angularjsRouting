4d7030db1354c0826cd59c9aa20325b4a1e838ce
Supriya <supriya.ps29@gmail.com>
2017-11-04 22:43:16 +0530

    lets review added

---
+++ Let's Review/Home.js
@@ -0,0 +1,8 @@
+/**
+ * Created by APPLE on 04-11-2017.
+ */
+
+var app=angular.module("myapp",[]);
+app.controller('myctrl',function($script){
+    $scope.names = ["ambiences", "Tobias", "Linus"];
+})
